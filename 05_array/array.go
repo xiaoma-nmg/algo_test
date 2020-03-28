69f3f77151335aed252b7a460f09ef9d1e3e472b
xiaoma_nmg <maqiang_nmg@163.com>
2020-03-29 00:20:23 +0800

    add array

--- 05_array/array.go
+++ 05_array/array.go
@@ -1,1 +1,81 @@
 package _5_array
+
+import (
+    "errors"
+    "fmt"
+)
+
+/**
+ * 1) 数组的插入、删除、按照下标随机访问操作；
+ * 2）数组中的数据是int类型的；
+ *
+ */
+
+
+type Array struct {
+    data []int
+}
+
+func NewArray(capacity uint) *Array{
+    if capacity == 0 {
+        return nil
+    }
+    return &Array{
+        data:make([]int, 0, capacity),
+    }
+}
+
+// 判断下标是否越界
+func (a *Array)IsIndexOutOfRange(index uint) bool {
+    return index >= uint(cap(a.data))
+}
+
+// 通过下标查找值，索引范围[0, n-1]
+func (a *Array)Find(index uint)(int, error) {
+    if a.IsIndexOutOfRange(index) {
+        return 0, errors.New("out of index range")
+    }
+    return a.data[index], nil
+}
+
+// 插入数据到索引index上
+func (a *Array)Insert(index uint, val int) error {
+    if len(a.data) == cap(a.data) {
+        return errors.New("array is full")
+    }
+
+    if a.IsIndexOutOfRange(index) {
+        return errors.New("out of index range")
+    }
+
+    if index == uint(len(a.data)) {
+        a.data = append(a.data, val)
+        return nil
+    }
+
+    copy(a.data[index+1:], a.data[index:])
+    a.data[index] = val
+    return nil
+}
+
+// 插入数据到数组尾部
+func (a *Array)InsertToTail(val int) error {
+    return a.Insert(uint(len(a.data)), val)
+}
+
+func (a *Array)Delete(index uint)(int, error)  {
+    if a.IsIndexOutOfRange(index) {
+        return 0, errors.New("out of index range")
+    }
+    val := a.data[index]
+    a.data = append(a.data[:index], a.data[index+1:]...)
+    return val, nil
+}
+
+func (a *Array)Print()  {
+    var format string
+    for _, num := range a.data {
+        format += fmt.Sprintf("|%+v", num)
+    }
+    fmt.Println(format)
+}
---
+++ 05_array/array_test.go
@@ -0,0 +1,73 @@
+package _5_array
+
+import (
+    "testing"
+)
+
+func TestInsert(t *testing.T) {
+    capacity := 10
+    arr := NewArray(uint(capacity))
+    for i := 0; i < capacity-2; i++ {
+        err := arr.Insert(uint(i), i+1)
+        if nil != err {
+            t.Fatal(err.Error())
+        }
+    }
+    arr.Print()
+
+    arr.Insert(uint(6), 999)
+    arr.Print()
+
+    arr.InsertToTail(666)
+    arr.Print()
+
+    err := arr.InsertToTail(888)
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+    arr.Print()
+
+    err = arr.Insert(uint(7), 333)
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+    arr.Print()
+}
+
+func TestDelete(t *testing.T) {
+    capacity := 10
+    arr := NewArray(uint(capacity))
+    for i := 0; i < capacity; i++ {
+        err := arr.Insert(uint(i), i+1)
+        if nil != err {
+            t.Fatal(err.Error())
+        }
+    }
+    arr.Print()
+
+    _, _ = arr.Delete(uint(6))
+    arr.Print()
+    //for i := 9; i >= 0; i-- {
+    //    _, err := arr.Delete(uint(i))
+    //    if nil != err {
+    //        t.Fatal(err)
+    //    }
+    //    arr.Print()
+    //}
+}
+
+func TestFind(t *testing.T) {
+    capacity := 10
+    arr := NewArray(uint(capacity))
+    for i := 0; i < capacity; i++ {
+        err := arr.Insert(uint(i), i+1)
+        if nil != err {
+            t.Fatal(err.Error())
+        }
+    }
+    arr.Print()
+
+    t.Log(arr.Find(0))
+    t.Log(arr.Find(9))
+    t.Log(arr.Find(11))
+}
